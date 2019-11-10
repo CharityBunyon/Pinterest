d7f78b8aca4c915f9d7cac31f0613193d6658099
Charity Bunyon <cseymou5@gmail.com>
2019-11-10 14:43:04 -0600

    added event listener on pin card btn

--- src/javascripts/components/Boards/boards.js
+++ src/javascripts/components/Boards/boards.js
@@ -1,18 +1,50 @@
 import './boards.scss';
 import $ from 'jquery';
-// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import utilities from '../../helpers/utilities';
 import boardData from '../../helpers/data/boardData';
 import boardCard from '../BoardCard/boardCard';
-import singleBoard from '../singleBoard/singleBoard';
-// import pinsData from '../../helpers/data/pinsData';
-// import pinsPrint from '../pins/pins';
+// import singleBoard from '../singleBoard/singleBoard';
+import pinsData from '../../helpers/data/pinData';
+import pinsPrint from '../PinCard/pinCard';
+
+const close = () => {
+  const { uid } = firebase.auth().currentUser;
+  $(document).click((e) => {
+    const buttonName = e.target.className;
+    if (buttonName === 'closeBtn') {
+      // eslint-disable-next-line no-use-before-define
+      buildBoards(uid);
+      // $('#newBoardButton').removeClass('hide');
+      // $('#newPinButton').addClass('hide');
+    }
+  });
+};
+
+const showSingleBoard = (boardId) => {
+  pinsData.getPinsByBoardId(boardId)
+    .then((pins) => {
+      console.log('here are the pins', pins);
+      let domString = '<div id="boardSection" class="d-flex flex-wrap"><span><button class=" btn btn-success btn-lg btn-block closeBtn">Close</button><span>';
+      pins.forEach((pin) => {
+        domString += pinsPrint.makePin(pin);
+      });
+      domString += '</div>';
+      utilities.printToDom('boards', domString);
+      $('#boardSection').on('click', '.closeBtn', close);
+    })
+    .catch((error) => console.error(error));
+};
 
 const createSingleBoard = (e) => {
   const boardId = e.target.id;
-  singleBoard.buildSingleBoard(boardId);
+  showSingleBoard(boardId);
 };
+
+// const deleteBoard = () => {
+
+// };
 
 const buildBoards = (uid) => {
   boardData.getBoards(uid)
@@ -25,6 +57,9 @@ const buildBoards = (uid) => {
       domString += '</div>';
       utilities.printToDom('boards', domString);
       $('#boards').on('click', '.chosen-board', createSingleBoard);
+      // $('#pins').on('click', '.close', close);
+
+      // boardsDiv.addClass('hide');
     })
     .catch((error) => console.error(error));
 };
--- src/javascripts/components/PinCard/pinCard.js
+++ src/javascripts/components/PinCard/pinCard.js
@@ -1,12 +1,15 @@
+import './pinCard.scss';
+
 const makePin = (pin) => {
   let domString = '';
   if (pin.id) {
     domString += `
-    <div class="card col pinCard"> 
+    <div class="card  d-flex flex-wrap container" id="pinCard">
     <h5 class="card-title">${pin.title}</h5>
       <div class="card-body">
-      <img src="${pin.imgUrl}" class="card-img-top" alt="...">
+      <img src="${pin.imgUrl}" class="" height="400px" width= "400px" alt="...">
         <p class="card-text">${pin.description}</p>
+        <button href="#" class="btn btn-primary deletePin" id="${pin.id}" databoardid="${pin.boardID}">Delete</button>
       </div>
     </div>
     `;
